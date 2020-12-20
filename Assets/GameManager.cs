a7dd63e8d4238fe9d1975b1e44775657cbc85653
Fernando <Figueroa>
2020-12-20 01:22:55 -0300

    Game Manager
    Necesito ayuda

---
+++ Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<ButtonMashingEvent> cats = new List<ButtonMashingEvent>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var allCharacters = FindObjectsOfType<ButtonMashingEvent>();
+
+        foreach (var myCharacter in allCharacters)
+        {
+            var player = myCharacter.GetComponent<ButtonMashingEvent>();
+
+            if (myCharacter.tag == "Player")
+                cats.Add(player);
+        }
+    }
+}
