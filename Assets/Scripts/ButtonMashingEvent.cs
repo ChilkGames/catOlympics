63f7b9a21ba76794650e88b2fefd64a1ff4e0a92
Cristian <cristianangelicchio@gmail.com>
2020-12-20 00:24:45 -0300

    Merged part of the minigame

--- Assets/Scripts/ButtonMashingEvent.cs
+++ Assets/Scripts/ButtonMashingEvent.cs
@@ -4,20 +4,7 @@ using UnityEngine;
 
 public class ButtonMashingEvent : MonoBehaviour
 {
-
-    public GameObject cat1;
-    public GameObject cat2;
-    public GameObject cat3;
-    public GameObject cat4;
-
     public float speed;
-    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -27,24 +14,9 @@ public class ButtonMashingEvent : MonoBehaviour
 
     void ButtonMashing()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            cat1.transform.position += new Vector3(10, 0, 0) * speed * Time.deltaTime;
-        }
-
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            cat2.transform.position += new Vector3(10, 0, 0) * speed * Time.deltaTime;
-        }
-
-        if (Input.GetKeyDown(KeyCode.O))
-        {
-            cat3.transform.position += new Vector3(10, 0, 0) * speed * Time.deltaTime;
-        }
-
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            cat4.transform.position += new Vector3(10, 0, 0) * speed * Time.deltaTime;
+            transform.position += new Vector3(10, 0, 0) * speed * Time.deltaTime;
         }
     }
 }
